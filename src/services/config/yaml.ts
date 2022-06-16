7a9e88a1342969bd8ea4ec9faf40bba6eb5552ef
赖其彬 <lqb@163.com>
2022-06-16 21:10:27 +0800

    feat: JsYamlConfigLoader

--- src/services/config/yaml.ts
+++ src/services/config/yaml.ts
@@ -4,12 +4,7 @@ import devConfig from 'js-yaml-loader!../../../config-it.yaml';
 import fs from 'fs';
 
 export default devConfig;
-fs.readFile('../../../config.yaml', 'utf-8', function (err, data) {
-  if (err) {
-    throw err
-  }
-  console.log('读取成功！！！');
-})
+console.log(fs.readFileSync('../../../config.yaml', 'utf-8'));
 
 console.log(process.env);
 
