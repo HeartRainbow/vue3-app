5fa2a7b3ebb853f2608b1bda64a75b7cc1fdd8fb
赖其彬 <799316642@qq.com>
2022-08-05 18:47:14 +0800

    feat: 提取时间处理方法到DateLoader中

---
+++ src/utils/date-loader/days-of-month.ts
@@ -0,0 +1,20 @@
+/**
+ * 根据某年某月计算出具体日期
+ * 
+ * @param year 年份
+ * @param month 月份
+ * @returns 
+ */
+function getDaysInMonth(year: string, month: string) {
+    const daysOfMonth = [];
+    const mon = parseInt(month, 10);
+    const lastDayOfMonth = new Date(Number(year), mon, 0).getDate();
+    for (let i = 1; i <= lastDayOfMonth; i++) {
+        if (i < 10) {
+            daysOfMonth.push(year + "-" + month + "-" + "0" + i);
+        } else {
+            daysOfMonth.push(year + "-" + month + "-" + i);
+        }
+    }
+    return daysOfMonth;
+}
